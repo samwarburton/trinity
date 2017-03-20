d7bf036bf43e5d74e8fe23fce19aba6c0a50fde1
samwarburton <samwarburon93@gmail.com>
2017-03-20 23:02:40 +1100

    Initial commit

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Project Information </div>
+                <div class="panel-heading">Projects </div>
                 <div class="panel-body">
 
                 <h3>{{$project->name}}</h3></br>
